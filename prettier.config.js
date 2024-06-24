2a000509506142ce01e00afe39e01b07bc88492b
Josh Harris <jdharrisnz@icloud.com>
2024-06-24 20:49:00 +0100

    Updated prettier

--- prettier.config.js
+++ prettier.config.js
@@ -1,12 +1,12 @@
 /** @type {import("prettier").Config} */
 
 const config = {
-    plugins: ["prettier-plugin-jsdoc"],
-    printWidth: 100,
-    proseWrap: "always",
-    semi: false,
-    tabWidth: 4,
-    trailingComma: "none"
+  plugins: ["prettier-plugin-jsdoc"],
+  printWidth: 80,
+  proseWrap: "always",
+  semi: false,
+  tabWidth: 2,
+  trailingComma: "none"
 }
 
 export default config

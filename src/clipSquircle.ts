eeefd2053a2a1020cf4fb76dc857da4a0fe28ada
Josh Harris <jdharrisnz@icloud.com>
2024-07-03 20:33:03 +0100

    Project restructure

---
+++ src/clipSquircle.ts
@@ -0,0 +1,11 @@
+import { Types } from "./types.js"
+import { getCurveSpec, getPath } from "./utils.js"
+
+/** Produces a squircle path to be used in `background-clip` inline styles. */
+export const clipSquircle = ({
+  width,
+  height,
+  curveLength,
+  roundness = 0.2
+}: Types.SquircleOptionsClip): `path('${string}')` =>
+  `path('${getPath(width, height, ...getCurveSpec(width, height, curveLength, roundness))}')`

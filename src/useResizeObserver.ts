62fca1f416a3f53b98703c096efafd01013ef2af
Josh Harris <jdharrisnz@icloud.com>
2024-07-05 11:38:35 +0100

    Fix issue with failing to react to all changes

--- src/useResizeObserver.ts
+++ src/useResizeObserver.ts
@@ -1,22 +1,55 @@
-import { useEffect } from "react"
+import { useLayoutEffect, useReducer } from "react"
 
 import type { RefObject } from "react"
 
+interface Size {
+  readonly width: number
+  readonly height: number
+}
+
+/** @internal */
+const sizeReducer = (
+  currentState: Size,
+  entries: readonly ResizeObserverEntry[]
+): Size =>
+  entries.reduce<Size>((acc, { target: { scrollWidth, scrollHeight } }) => {
+    // Round pixels to avoid overly-frequent re-renders
+    const width = Math.round(scrollWidth)
+    const height = Math.round(scrollHeight)
+
+    // Don't update state if the result is the same
+    return width === acc.width && height === acc.height
+      ? acc
+      : {
+          width,
+          height
+        }
+  }, currentState)
+
+/** @internal */
+const initialState: Size = {
+  width: 0,
+  height: 0
+}
+
 /** @internal */
 export const useResizeObserver = <T extends Element>(
-  callback: ResizeObserverCallback,
   ref: RefObject<T>
-): void => {
-  useEffect(() => {
-    const { current } = ref
+): Size => {
+  const [size, callback] = useReducer(sizeReducer, initialState)
 
-    if (!current) return
+  // No dependencies as we want to re-run on every render
+  useLayoutEffect(() => {
+    if (!ref.current) return
 
     const observer = new ResizeObserver(callback)
-    observer.observe(current)
+
+    observer.observe(ref.current)
 
     return () => {
-      observer.unobserve(current)
+      observer.disconnect()
     }
-  }, [callback, ref])
+  })
+
+  return size
 }

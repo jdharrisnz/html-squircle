5e789c1d918d9cc37556a45d5dbf4cf54ef1bf69
Josh Harris <jdharrisnz@icloud.com>
2024-08-09 09:15:06 +0100

    Export types

--- dist/dts/index.d.ts
+++ dist/dts/index.d.ts
@@ -3,4 +3,5 @@ export { backgroundSquircle } from "./core/backgroundSquircle.js";
 export { clipSquircle } from "./core/clipSquircle.js";
 export { useBackgroundSquircle } from "./hooks/useBackgroundSquircle.js";
 export { useClipSquircle } from "./hooks/useClipSquircle.js";
+export type { Types } from "./types.js";
 //# sourceMappingURL=index.d.ts.map

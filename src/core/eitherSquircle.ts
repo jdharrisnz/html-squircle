603897ff6cc936f1934bf271fb33fae02fea1925
Josh Harris <jdharrisnz@icloud.com>
2025-01-03 11:38:21 +1300

    Rename types and reorder function overloads

--- src/core/eitherSquircle.ts
+++ src/core/eitherSquircle.ts
@@ -3,20 +3,28 @@ import { clipSquircleObj } from "./clipSquircle.js"
 
 import type { Types } from "../types.js"
 
-type OptionsSvgKeys = Exclude<
+type OptionsBackgroundKeys = Exclude<
   keyof Types.SquircleOptionsBackground,
   keyof Types.SquircleOptionsClip
 >
 
-const isOptionsSVG = (
+const isOptionsBackground = (
   opts: Types.SquircleOptionsClip | Types.SquircleOptionsBackground,
 ): opts is Types.SquircleOptionsBackground =>
-  ("stroke" satisfies OptionsSvgKeys) in opts ||
-  ("strokeWidth" satisfies OptionsSvgKeys) in opts ||
-  ("background" satisfies OptionsSvgKeys) in opts ||
-  ("injectedDefs" satisfies OptionsSvgKeys) in opts ||
-  ("injectedBody" satisfies OptionsSvgKeys) in opts
+  ("stroke" satisfies OptionsBackgroundKeys) in opts ||
+  ("strokeWidth" satisfies OptionsBackgroundKeys) in opts ||
+  ("background" satisfies OptionsBackgroundKeys) in opts ||
+  ("injectedDefs" satisfies OptionsBackgroundKeys) in opts ||
+  ("injectedBody" satisfies OptionsBackgroundKeys) in opts
 
+/**
+ * Overloaded function for getting either the background or clip squircle
+ * objects. Returns a result based on whether any background options are present
+ * in the options object.
+ */
+export function eitherSquircleObj(
+  options: Types.SquircleOptionsClip,
+): ReturnType<typeof clipSquircleObj>
 /**
  * Overloaded function for getting either the background or clip squircle
  * objects. Returns a result based on whether any background options are present
@@ -25,15 +33,17 @@ const isOptionsSVG = (
 export function eitherSquircleObj(
   options: Types.SquircleOptionsBackground,
 ): ReturnType<typeof backgroundSquircleObj>
-export function eitherSquircleObj(
-  options: Types.SquircleOptionsClip,
-): ReturnType<typeof clipSquircleObj>
+/**
+ * Overloaded function for getting either the background or clip squircle
+ * objects. Returns a result based on whether any background options are present
+ * in the options object.
+ */
 export function eitherSquircleObj(
   options: Types.SquircleOptionsClip | Types.SquircleOptionsBackground,
 ):
   | ReturnType<typeof clipSquircleObj>
   | ReturnType<typeof backgroundSquircleObj> {
-  return isOptionsSVG(options) ?
+  return isOptionsBackground(options) ?
       backgroundSquircleObj(options)
     : clipSquircleObj(options)
 }
